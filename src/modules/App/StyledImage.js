0ca0077301b5183d343245ad3abdc19a0c9e2b42
Bill Hefty <billhefty@gmail.com>
2017-03-04 08:57:47 -0600

    add margin-bottom of 20px

--- src/modules/App/StyledImage.js
+++ src/modules/App/StyledImage.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { Image } from 'react-bootstrap'
 
 const StyledImage = styled(Image)`
-    margin: 0 auto;
+    margin: 0 auto 20px auto;
 `
 
 export default StyledImage
