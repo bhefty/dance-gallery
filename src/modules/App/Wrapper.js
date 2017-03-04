af8dc08f9ac6c2bcb2be65a671455b7307e016fb
Bill Hefty <billhefty@gmail.com>
2017-03-04 09:12:14 -0600

    Change fill style for children components

--- src/modules/App/Wrapper.js
+++ src/modules/App/Wrapper.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-    padding: 0 2em;
+    background-color: rgba(255, 255, 255, 0.6);
+    border: 2px solid rgba(255, 255, 255, 0.8);
+    margin: 2em;
+    padding: 1em;
 `
 
 export default Wrapper
--- src/modules/App/index.js
+++ src/modules/App/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
 
 import Navigation from './components/Navigation'
 
@@ -13,13 +12,14 @@ const App = (props) => {
       <Navigation />
       <StyledImage src={building} alt='The Dance Gallery Building' responsive />
       <Wrapper>
-        <Row>
-          <Col xs={12}>
-            
-          </Col>
-        </Row>
         <p>
-          Sed ante ligula, pellentesque id orci vitae, lacinia faucibus metus. Proin dolor ligula, efficitur sed quam sed, imperdiet mattis risus. Aenean convallis dui vel neque suscipit, eu viverra urna sagittis. Maecenas sit amet turpis ultrices neque lacinia elementum. Morbi venenatis dapibus ex, a porta justo posuere ac. Aliquam et nisl vestibulum, efficitur mi et, convallis nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus commodo quam turpis, in tincidunt tellus gravida sed. Proin dolor erat, laoreet sit amet posuere quis, ultricies eget orci. Vivamus nunc elit, vestibulum vitae velit sed, imperdiet scelerisque ante. Vivamus blandit nibh non faucibus eleifend. Maecenas ultrices justo nec erat aliquam hendrerit. Duis eget rutrum dui, sed consectetur lacus. Sed fermentum leo in turpis tristique rutrum. Pellentesque finibus interdum orci ut rutrum. Etiam posuere est libero, sed sollicitudin sapien sollicitudin vitae.
+          Vivamus feugiat velit vitae odio sagittis pellentesque. Aliquam porta orci eu odio venenatis, non aliquam lectus pharetra. Quisque mi lacus, sollicitudin aliquam turpis ac, interdum euismod lorem. Etiam in consequat quam. Suspendisse potenti. Sed et dolor ut urna fermentum eleifend. Sed eget vulputate mauris, eget ultrices lacus. Nulla rutrum pretium commodo. Praesent auctor egestas mauris, quis iaculis nisl pulvinar sollicitudin. Pellentesque facilisis, magna vel interdum lobortis, quam ex laoreet arcu, sit amet vulputate arcu nulla in neque. Pellentesque ut purus ut lectus efficitur tincidunt vel ut urna. Vivamus tristique vehicula felis ut vehicula.
+        </p>
+        <p>
+          Vivamus feugiat velit vitae odio sagittis pellentesque. Aliquam porta orci eu odio venenatis, non aliquam lectus pharetra. Quisque mi lacus, sollicitudin aliquam turpis ac, interdum euismod lorem. Etiam in consequat quam. Suspendisse potenti. Sed et dolor ut urna fermentum eleifend. Sed eget vulputate mauris, eget ultrices lacus. Nulla rutrum pretium commodo. Praesent auctor egestas mauris, quis iaculis nisl pulvinar sollicitudin. Pellentesque facilisis, magna vel interdum lobortis, quam ex laoreet arcu, sit amet vulputate arcu nulla in neque. Pellentesque ut purus ut lectus efficitur tincidunt vel ut urna. Vivamus tristique vehicula felis ut vehicula.
+        </p>
+        <p>
+          Vivamus feugiat velit vitae odio sagittis pellentesque. Aliquam porta orci eu odio venenatis, non aliquam lectus pharetra. Quisque mi lacus, sollicitudin aliquam turpis ac, interdum euismod lorem. Etiam in consequat quam. Suspendisse potenti. Sed et dolor ut urna fermentum eleifend. Sed eget vulputate mauris, eget ultrices lacus. Nulla rutrum pretium commodo. Praesent auctor egestas mauris, quis iaculis nisl pulvinar sollicitudin. Pellentesque facilisis, magna vel interdum lobortis, quam ex laoreet arcu, sit amet vulputate arcu nulla in neque. Pellentesque ut purus ut lectus efficitur tincidunt vel ut urna. Vivamus tristique vehicula felis ut vehicula.
         </p>
       </Wrapper>
     </div>
